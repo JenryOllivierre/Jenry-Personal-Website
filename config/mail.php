ac407219548bff5ba10c05286d532f04469d0539
Jenry Ollivierre <jenry@jenryollivierre.com>
2019-05-09 16:13:01 -0400

    Added admin/webmaster mail address and name

--- config/mail.php
+++ config/mail.php
@@ -57,7 +57,12 @@ return [
 
     'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'name' => env('MAIL_FROM_NAME', 'hello'),
+    ],
+
+    'admin' => [
+        'address' => env('MAIL_TO_ADDRESS', 'hello@example.com'),
+        'name' => env('MAIL_TO_NAME', 'hello'),
     ],
 
     /*
